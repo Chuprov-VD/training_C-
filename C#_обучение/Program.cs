0f6cc38f075170f020b3f512281b776ab7d642f3
chuprov-vd <chuprov271@gmail.com>
2023-03-07 00:33:40 +0300

    add new code

--- C#_обучение/Program.cs
+++ C#_обучение/Program.cs
@@ -1,5 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi!");
-Console.WriteLine("Hi!");
-Console.WriteLine("Hi!");
+﻿// Вывод через консоль приветствие Console - обращение к консоле; 
+//.WriteLin - команда для печати на экран
+//Console.WriteLine("Hello, World!"); 
+
+// Просьба ввести данные у пользователя и поприветствовать его
+// string - тип данных (строка) добавляется перед переменной
+string username = Console.ReadLine();
+// username - имя пользователя; Console - обращение к консоле; .Readline - команда для ввода данных
+Console.WriteLine("Привет, "); // выводит - Привет, 
+Console.WriteLine(username); // выводит содержание переменной username
