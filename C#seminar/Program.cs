eb2eb68fddc149829ce4aa8fd73dd568b02024a9
chuprov-vd <chuprov271@gmail.com>
2023-03-12 00:45:03 +0300

    2 задача

--- C#seminar/Program.cs
+++ C#seminar/Program.cs
@@ -1,7 +1,27 @@
-﻿Console.Write("Введите 1 число: ");
+﻿// 1 задача Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//Console.Write("Введите 1 число: ");
+//int a = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 преобразует данные к нужному типу
+//Console.Write("Введите 2 число: ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//int max = a;
+//if (max > b)
+//{
+//  max = a;
+//}
+//if (max < b)
+//{
+//  max = b;
+//}
+//Console.Write("Максимум = ");
+//Console.WriteLine(max);
+
+// 2 задача  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.Write("Введите 1 число: ");
 int a = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 преобразует данные к нужному типу
 Console.Write("Введите 2 число: ");
 int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3 число: ");
+int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
 if (max > b)
 {
@@ -10,6 +30,10 @@ if (max > b)
 if (max < b)
 {
     max = b;
+}
+if (max < c)
+{
+    max = c;
 }
 Console.Write("Максимум = ");
 Console.WriteLine(max);
