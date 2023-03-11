d360e99b1f99e41b20fdff575b2c1b8f1c96ec9f
chuprov-vd <chuprov271@gmail.com>
2023-03-12 00:37:06 +0300

    1 задача

--- C#seminar/Program.cs
+++ C#seminar/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите 1 число: ");
+int a = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 преобразует данные к нужному типу
+Console.Write("Введите 2 число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max > b)
+{
+    max = a;
+}
+if (max < b)
+{
+    max = b;
+}
+Console.Write("Максимум = ");
+Console.WriteLine(max);
