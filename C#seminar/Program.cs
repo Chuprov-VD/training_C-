584446c29c991cbc51c9910b1ee5e60dc4cb68e0
chuprov-vd <chuprov271@gmail.com>
2023-04-08 01:34:45 +0300

    7 dz seminar

--- C#seminar/Program.cs
+++ C#seminar/Program.cs
@@ -1,85 +1,124 @@
-﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-//Напишите программу, которая покажет количество чётных чисел в массиве.
-//[345, 897, 568, 234] -> 2
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-void InputArray(int[] array, ref int count) // процедура void
+Console.Clear();
+
+void PrintArry(double[,] matr) //(метод для вывода матрицы)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int rows = 0; rows < matr.GetLength(0); rows++) // matrix.GetLength(0) - обращается к количесвту строк
     {
-        array[i] = new Random().Next(100, 1000);
-        if (array[i] % 2 == 0)
+        for (int columns = 0; columns < matr.GetLength(1); columns++) //GetLength(1) - обращается к количеству столбцов 
         {
-            count++; // считает все четные числа
+            Console.Write($"{matr[rows, columns]} "); // для вывода строки столбца
+        }
+        Console.WriteLine(); // пробел для красивого вывода 2 строки и так далее
+    }
+}
+
+
+void FillArray(double[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++) // matrix.GetLength(0) - обращается к количесвту строк
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++) //GetLength(1) - обращается к количеству столбцов 
+        {
+            int num = new Random().Next(-9, 10);
+            matr[rows, columns] = Math.Round(new Random().NextDouble() * num, 2);
         }
     }
 }
 
+double[,] matrix = new double[3, 4];
+FillArray(matrix);
+PrintArry(matrix);
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+//17 -> такого числа в массиве нет
+
 Console.Clear();
-Console.Write($"Введите число которое задаст, количество чисел в массиве: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[number]; // создаем пустой массив
-int count = 0;
-InputArray(array, ref count); // вызов метода + ссылка на счет(на массив ссылку можно не добовлять)
-Console.WriteLine($"[{string.Join(", ", (array))}] -> {count}"); // string.Join для передачи 
-//процедуры void, и для добавления запятых
 
-//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
-//Найдите сумму элементов, стоящих на нечётных позициях.
-
-//[3, 7, 23, 12] -> 19
-//[-4, -6, 89, 6] -> 0
-
-void InputArray(int[] array, ref int count) // процедура void
+void PrintArry(int[,] matr, int num, int flag) //(метод для вывода матрицы)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int rows = 0; rows < matr.GetLength(0); rows++) // matrix.GetLength(0) - обращается к количесвту строк
     {
-        array[i] = new Random().Next(-100, 100);
-        if (array[i] % 2 != 0)
+        for (int columns = 0; columns < matr.GetLength(1); columns++) //GetLength(1) - обращается к количеству столбцов 
         {
-            count = array[i] + count;
+            Console.Write($"{matr[rows, columns]} "); // для вывода строки столбца
+            if (matr[rows, columns] == num) flag++;
+        }
+        Console.WriteLine(); // пробел для красивого вывода 2 строки и так далее
+    }
+    if (flag == 0) Console.WriteLine($"{num} -> такого числа в массиве нет");
+    else Console.WriteLine($"{num} -> такое число в массиве есть, оно повторяется {flag} раз");
+}
+
+
+void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++) // matrix.GetLength(0) - обращается к количесвту строк
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++) //GetLength(1) - обращается к количеству столбцов 
+        {
+            matr[rows, columns] = new Random().Next(1, 30);
         }
     }
 }
 
-Console.Clear();
-Console.Write($"Введите число которое задаст, количество чисел в массиве: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[number];
-int count = 0;
-InputArray(array, ref count); // вызов метода + ссылка на счет(на массив ссылку можно не добовлять)
-Console.WriteLine($"[{string.Join(", ", (array))}] -> {count}");
+int[,] matrix = new int[3, 4];
+Console.Write("Ввыедите число для поиска: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int flag = 0;
+FillArray(matrix);
+PrintArry(matrix, num, flag);
 
-//Задача 38: Задайте массив вещественных чисел. 
-//Найдите разницу между максимальным и минимальным элементов массива.
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
 
-//[3 7 22 2 78] -> 76
-
-void InputArray(double[] array, ref double max, ref double min) // процедура void
+void FillArray(int[,] matr, int line, double[] array)
 {
-
-    for (int i = 0; i < array.Length; i++)
+    for (int columns = 0; columns < matr.GetLength(1); columns++) // matrix.GetLength(0) - обращается к количесвту строк
     {
-        array[i] = new Random().Next(-100, 100);
-        if (array[i] > max)
+        double num = 0;
+        for (int rows = 0; rows < matr.GetLength(0); rows++) //GetLength(1) - обращается к количеству столбцов 
         {
-            max = array[i];
+            matr[rows, columns] = new Random().Next(1, 10);
+            num = matr[rows, columns] + num;
         }
+        array[columns] = Math.Round(num / line, 1);
     }
-    min = max;
-    for (int i = 0; i < array.Length; i++)
+    Console.WriteLine($"Среднее арифметическое каждого столбца: {string.Join("; ", array)}; {line}");
+}
+void PrintArry(int[,] matr) //(метод для вывода матрицы)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++) // matrix.GetLength(0) - обращается к количесвту строк
     {
-        if (array[i] < min)
+        for (int columns = 0; columns < matr.GetLength(1); columns++) //GetLength(1) - обращается к количеству столбцов 
         {
-            min = array[i];
+            Console.Write($"{matr[rows, columns]} "); // для вывода строки столбца
         }
+        Console.WriteLine(); // пробел для красивого вывода 2 строки и так далее
     }
 }
 
+
 Console.Clear();
-Console.Write($"Введите число которое задаст, количество чисел в массиве: ");
-int number = Convert.ToInt32(Console.ReadLine());
-double[] array = new double[number];
-double max = 0;
-double min = 100;
-InputArray(array, ref max, ref min); // вызов метода + ссылка на счет(на массив ссылку можно не добовлять)
-Console.WriteLine($"[{string.Join(" ", (array))}] -> {max - min}");
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[rows, columns];
+double[] array = new double[columns];
+
+
+FillArray(matrix, rows, array);
+PrintArry(matrix);
